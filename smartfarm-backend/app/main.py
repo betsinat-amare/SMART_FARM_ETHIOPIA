99cc5aa6c9c1737ceb19ef28ef7febeb342a6508
betsinat-amare <amarebetsinat@gmail.com>
2026-03-06 10:51:31 +0300

    updated the service logic for crop service

--- smartfarm-backend/app/main.py
+++ smartfarm-backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine
 from app.routes import auth
 from app.routes import users
@@ -8,6 +9,14 @@ from app.routes import crop
 from app.routes import prediction
 
 app = FastAPI(title="SmartFarm Ethiopia API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth.router)
